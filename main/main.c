8fba319043b8752cb042b9e0e6a8a4c964e537ac
CRWolff <27215580+crwolff@users.noreply.github.com>
2024-07-10 17:04:26 -0600

    Output key sequence to select boot mode

--- main/main.c
+++ main/main.c
@@ -12,6 +12,8 @@
 #include <string.h>
 #include "esp_event.h"
 #include <esp_log.h>
+#include "tinyusb.h"
+#include "usb_descriptors.h"
 
 static const char *TAG = "webster";
 
@@ -22,9 +24,14 @@ bool wifi_isup(void);
 void wifi_init(void);
 void httpd_init(void);
 
+extern uint32_t button_pressed;
+
 /* Main application */
 void app_main(void)
 {
+    uint8_t sequence;
+    uint8_t key_active;
+
     // Turn on event loop
     ESP_ERROR_CHECK(esp_event_loop_create_default());
 
@@ -40,12 +47,61 @@ void app_main(void)
     // Start web server
     httpd_init();
 
-    // Watch for connection loss and try reconnect
+    // Main task - loop forever
     while (1) {
+        // Watch for connection loss and try reconnect
         vTaskDelay(pdMS_TO_TICKS(1000));
         if (!wifi_isup()) {     // WiFi failed, re-init
             ESP_LOGI(TAG, "WiFi down, attempting restart");
             wifi_init();
+        }
+
+        // Web server sets button_pressed based on POST command
+        // Record button_pressed so it can't change during sequence
+        uint32_t btn = button_pressed;
+        if ( btn ) {
+            // Send key sequence
+            //     30 spaces (halts grub autoboot)
+            //     n down arrows corresponding to button number
+            //     ENTER to start boot
+            sequence = 33;
+            key_active = 0;
+            for(int i=0;(i < 6000) && (sequence != 0);i++) {
+                if ( tud_suspended() ) {
+                    // Wake up host if we are in suspend mode
+                    // and REMOTE_WAKEUP feature is enabled by host
+                    tud_remote_wakeup();
+                }
+
+                // Send next keypress in sequence
+                if ( tud_hid_ready() ) {
+                    if ( key_active == 0 ) {
+                        uint8_t keycode[6] = { 0 };
+                        if ( sequence == 1 ) {
+                            keycode[0] = HID_KEY_ENTER;
+                        } else if ( sequence <= btn ) {
+                            keycode[0] = HID_KEY_ARROW_DOWN;
+                        } else {
+                            keycode[0] = HID_KEY_SPACE;
+                        }
+                        tud_hid_keyboard_report(REPORT_ID_KEYBOARD, 0, keycode);
+                        key_active = 1;
+                        vTaskDelay(pdMS_TO_TICKS(10));
+                    } else {
+                        tud_hid_keyboard_report(REPORT_ID_KEYBOARD, 0, NULL);
+                        sequence = sequence - 1;
+                        key_active = 0;
+                        vTaskDelay(pdMS_TO_TICKS(500));
+                    }
+                } else {
+                    vTaskDelay(pdMS_TO_TICKS(10));
+                }
+            }
+            if (sequence != 0)
+                printf("Timeout before sequence ended\n");
+
+            // Clear command and discard any that occurred during execution
+            button_pressed = 0;
         }
     }
 }
